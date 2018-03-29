138dafaa2352f107784107d655f1f9d4ddc1552a
Valeria <ava.killer.25@yandex.ru>
2018-03-29 16:09:03 +0700

    И еще кое-что поменяли. Теперь точно работает

--- public/index.php
+++ public/index.php
@@ -15,12 +15,12 @@ $router -> add('', ['controller' => 'Home', 'action' => 'index']);
 $router -> add('login', ['controller' => 'Home', 'action' => 'login']);
 $router -> add('register', ['controller' => 'Register', 'action' => 'reg']);
 $router -> add('shop', ['controller' => 'Shop', 'action' => 'index']);
-$router -> add('/admin/enter', ['admin/controller' => 'Enter', 'action' => 'login']);
-$router -> add('/admin/users', ['admin/controller' => 'Users', 'action' => 'index']);
-$router -> add('/admin/users/edit', ['admin/controller' => 'Users', 'action' => 'edit']);
-$router -> add('/admin/users/delete', ['admin/controller' => 'Users', 'action' => 'delete']);
-$router -> add('/admin/new', ['admin/controller' => 'New', 'action' => 'create']);
-$router -> add('/admin/stats', ['admin/controller' => 'Stats', 'action' => 'index']);
+$router -> add('admin/enter', ['admin/controller' => 'Enter', 'action' => 'login']);
+$router -> add('admin/users', ['admin/controller' => 'Users', 'action' => 'index']);
+$router -> add('admin/users/edit', ['admin/controller' => 'Users', 'action' => 'edit']);
+$router -> add('admin/users/delete', ['admin/controller' => 'Users', 'action' => 'delete']);
+$router -> add('admin/new', ['admin/controller' => 'New', 'action' => 'create']);
+$router -> add('admin/stats', ['admin/controller' => 'Stats', 'action' => 'index']);
 
 
 
