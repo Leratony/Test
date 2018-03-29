e7d974d406a51636bb78dd44c88f5ccdabc2fa5d
Valeria <ava.killer.25@yandex.ru>
2018-03-29 17:08:20 +0700

    Добавлен контроллер домашней страницы с двумя методами - index  и login

---
+++ App/Controllers/Home.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Home controller
+ * 
+ * PHP version  7.0.28
+ */
+
+ class Home
+ {
+     public function index()
+     {
+         echo "Here will be a home (main) page";
+     }
+
+     public function login()
+     {
+         echo "Here will be a redirect on home page after authorisation procedure";
+     }
+
+ }
+
+
+
+
+?>
