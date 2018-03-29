3304ef0748ba0607dc82368de89676e1c5b02a9f
Valeria Antonova <valeria.antonia001@gmail.com>
2018-03-29 20:45:23 +0600

    Добавили фильтры на странице

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -10,12 +10,29 @@ namespace App\Controllers;
 
  class Home extends \Core\Controller
  {
-     public function index()
+    protected function before()
+    {
+        echo "(before)"; // это те методы, которые вызываются до запуска какого-то контроллера.
+        // тут надо бы поместить авторизацию, наверное
+        // return false; - если это выставить, то пользователь не сможет видеть страницу, не авторизовавшись, например
+    }
+
+    protected function after()
+    {
+        echo "(after)"; // это те методы, которые вызываются после какого-то контроллера
+    }
+
+
+
+
+
+
+     public function indexAction()
      {
          echo "Here will be a home (main) page";
      }
 
-     public function login()
+     public function loginAction()
      {
          echo "Here will be a redirect on home page after authorisation procedure";
      }
