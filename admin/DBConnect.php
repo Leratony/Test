6562a81841176b682dcb0facc06ecd3ae717313b
Valeria <ava.killer.25@yandex.ru>
2018-03-29 13:54:56 +0700

    Сносим старое. Делаем почти с нуля

--- admin/DBConnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-mysql_connect("localhost", "admin_bd", "251502html") or die ("Unable connection" . mysql_error());
-
-mysql_query(" SET NAMES 'utf8");
-mysql_select_db("Users_Data") or die ("There is not this table!");
-	
-	
-?>	
--- admin/Delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('DBConnect.php');
-$id = $_GET['id'];
-$delete_q = mysql_query(" DELETE FROM Users WHERE id = '$id' ");
-echo "Note deleted successfully";
-//  header("Location: Main.php?page=home");
- 
- ?>
