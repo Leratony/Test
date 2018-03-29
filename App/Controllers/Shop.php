9442b1846759bde80ab92c10eea8dd286c81f969
Valeria Antonova <valeria.antonia001@gmail.com>
2018-03-29 19:44:44 +0600

    Добавили класс, проверили что там хорошо забираются кастомные параметры из адресной строки

--- App/Controllers/Shop.php
+++ App/Controllers/Shop.php
@@ -8,12 +8,19 @@ namespace App\Controllers;
  * PHP version  7.0.28
  */
 
- class Shop
+ class Shop extends \Core\Controller
  {
      public function index()
      {
          echo "Here will be a shop page";
          echo '<pre>Query string parameters:<pre>' . htmlspecialchars(print_r($_GET, true)) . '</pre></p>';
+     }
+
+     public function edit() //это не нужный метод, только для примера. Надо применить его в Users/index
+     {
+         echo 'Here you may edit some good';
+         echo '<p>Route parameters: <pre>' . 
+         htmlspecialchars(print_r($this->route_params, true)) . '</pre></p>';
      }
  }
 
