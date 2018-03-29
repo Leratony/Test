ebe2d4404d56d94aa2156e6ec81eb026be341ce0
Valeria Antonova <valeria.antonia001@gmail.com>
2018-03-30 01:39:18 +0600

    Добавлен контроллер входа в панель администратора

---
+++ App/Controllers/Admin/Login.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+/**
+ * Admin/Login controller
+ * 
+ * PHP version  7.0.28
+ */
+
+ class Login extends \Core\Controller
+ {
+    protected function before()
+    {
+        echo "(before)"; // это те методы, которые вызываются до запуска какого-то контроллера.
+        // тут надо убедиться в авторизации
+        // проверить, запущена ли сессия
+        // куки
+        // смена языка мб
+        // return false; - если это выставить, то пользователь не сможет видеть страницу, не авторизовавшись, например
+    }
+
+    protected function after()
+    {
+        echo "(after)"; // это те методы, которые вызываются после запуска какого-то контроллера.
+        // тут надо убедиться в авторизации
+        // еще какие-нибудь проверки
+        // return false; - если это выставить, то пользователь не сможет видеть страницу, не авторизовавшись, например
+    }
+
+
+
+
+
+     public function indexAction()
+     {
+         echo "Here you can log in admin panel";
+     }
+
+     
+
+     
+
+ }
+
+
+
+
+?>
