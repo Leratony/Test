c26c1d90fa8a36d8f02838adf5445dbd4ba51303
Valeria <ava.killer.25@yandex.ru>
2018-03-30 15:11:18 +0700

    Создан шаблон для вывода сообщений об удалении юзера

--- App/Controllers/Admin/Users.php
+++ App/Controllers/Admin/Users.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers\Admin;
 
+use \Core\View;
+
 /**
  * Admin/Users controller
  * 
@@ -12,14 +14,14 @@ namespace App\Controllers\Admin;
  {
     protected function before()
     {
-        echo "(before)"; // это те методы, которые вызываются до запуска какого-то контроллера.
+        // echo "(before)"; // это те методы, которые вызываются до запуска какого-то контроллера.
         // тут надо убедиться в авторизации
         // return false; - если это выставить, то пользователь не сможет видеть страницу, не авторизовавшись, например
     }
 
     protected function after()
     {
-        echo "(after)"; // это те методы, которые вызываются после запуска какого-то контроллера.
+        // echo "(after)"; // это те методы, которые вызываются после запуска какого-то контроллера.
         // тут надо убедиться в авторизации, сессии, куки, все дела
         // тут можно собрать какую-нибудь статистику, но отложим это дело
         // return false; - если это выставить, то пользователь не сможет видеть страницу, не авторизовавшись, например
@@ -28,22 +30,23 @@ namespace App\Controllers\Admin;
 
      public function indexAction()
      {
-         echo "Here will be a list of users";
+         View::renderTemplate('AdminUsers/index.html');
      }
 
      public function editAction()
      {
-         echo "Here will be a page for edition of users";
+         View::renderTemplate('AdminUsers/edit.html');
      }
 
      public function deleteAction()
      {
-         echo "Here will be a page with message about deleting progress";
+        //  echo "Here will be a page with message about deleting progress";
+         View::renderTemplate('AdminUsers/delete.html');
      }
 
      public function newAction()
      {
-         echo "Here will be a page for creating a new user";
+         View::renderTemplate('AdminUsers/new.html');
      }
 
      
