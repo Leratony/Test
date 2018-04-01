99e48541a3bf71be2235296cb698fc48e1eeb3fc
Valeria Antonova <valeria.antonia001@gmail.com>
2018-04-02 02:28:17 +0600

    Добавим параметры id в строку запросов админки

--- public/index.php
+++ public/index.php
@@ -52,6 +52,7 @@ $router->add('{controller}/{action}');
 $router -> add('{controller}/{id:\d+}/{action}');
 $router->add('admin/{controller}/{action}');
 $router -> add('admin/{controller}/{action}', ['namespace' => 'Admin']);
+$router -> add('admin/{controller}/{id:\d+}/{action}', ['namespace' => 'Admin']);
 
 // // Display the routing table
 
