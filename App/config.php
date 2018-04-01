4fcc060382a2ceb94b9ea2786c7263ca77c02161
Valeria Antonova <valeria.antonia001@gmail.com>
2018-04-01 23:02:54 +0600

    Файл конфигурации для базы данных

---
+++ App/config.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+// Configuration
+
+class Config 
+{
+    const DB_CONST = 'localhost';
+    const DB_NAME = 'Users_Data';
+    const DB_USER = 'admin_bd';
+    const DB_PASSWORD = '251502html'; 
+}
+
+?>
