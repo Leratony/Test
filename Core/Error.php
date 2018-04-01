27ee05d097b6f63978d76ebace1d80cc8fab23fc
Valeria Antonova <valeria.antonia001@gmail.com>
2018-04-02 00:27:22 +0600

    Добавили указание на использование конфига

--- Core/Error.php
+++ Core/Error.php
@@ -1,5 +1,6 @@
 <?php
 namespace Core;
+use App\Config;
 
 /**
  * Error handler
@@ -18,11 +19,26 @@ namespace Core;
 
     public static function exceptionHandler($exception)
     {
-        echo "<h1>Fatal error</h1>";
-        echo "<p>Uncaught exception: '" . get_class($exception) . "'</p>";
-        echo "<p>Message: '" . $exception->getMessage() . "'</p>";
-        echo "<p>Stack trace:<pre>" . $exception->getTraceAsString() . "</pre></p>";
-        echo "<p>Thrown in '" . $exception->getFile() . "' on line" . $exception->getLine() .  "</p>";
+        if (\App\Config::SHOW_ERRORS){
+            echo "<h1>Fatal error</h1>";
+            echo "<p>Uncaught exception: '" . get_class($exception) . "'</p>";
+            echo "<p>Message: '" . $exception->getMessage() . "'</p>";
+            echo "<p>Stack trace:<pre>" . $exception->getTraceAsString() . "</pre></p>";
+            echo "<p>Thrown in '" . $exception->getFile() . "' on line" . $exception->getLine() .  "</p>"; 
+        } else {
+            $log = dirname(__DIR__) . '/logs/' . date('Y-m-d') . 'txt';
+            ini_set('error_log', $log);
+
+            $message = "Uncaught exception: '" . get_class($exception) . "'";
+            $message .= " with message '" . $exception->getMessage() . "'";
+            $message .= "n\Stack trace: " . $exception->getTraceAsString();
+            $message .= "n\Thrown in '" . $exception->getFile() . "' on line " . $exception->getLine();
+            
+            error_log($message);
+            echo "<h1>An error occurred</h1>";
+
+        }
+        
     }
 
 
