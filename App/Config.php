2ba2c8495e237704ba60f91196c304ef226d17ec
Valeria Antonova <valeria.antonia001@gmail.com>
2018-04-02 01:08:54 +0600

    ok

--- App/Config.php
+++ App/Config.php
@@ -10,7 +10,7 @@ class Config
     const DB_NAME = 'Users_Data';
     const DB_USER = 'admin_bd';
     const DB_PASSWORD = '251502html'; 
-    //const SHOW_ERRORS = true;
+    // const SHOW_ERRORS = true;
     const SHOW_ERRORS = false;
 }
 
